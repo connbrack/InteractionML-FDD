6a7f9088ffabcf2464c50184065d445cb40f7575
connbrack <connor.brackley@mail.concordia.ca>
2023-09-10 18:36:38 -0400

    created popup box for selected data

--- website/src/routes/dataSelect/+page.svelte
+++ website/src/routes/dataSelect/+page.svelte
@@ -1,130 +1,97 @@
 <script>
+// Imports
+import chart from '$lib/highcharts';
+import Highcharts from 'highcharts';
+import {
+    Button,
+    Dropdown,
+    DropdownItem,
+    Spinner,
+    Toggle,
+    Modal,
+    Label,
+    Toast,
+    Textarea,
+    Radio,
+    Alert
+} from 'flowbite-svelte';
 
-  // Imports
-  import chart from '$lib/highcharts';
-  import Highcharts from 'highcharts';
-  import { Button, Dropdown, DropdownItem ,Spinner, Badge } from 'flowbite-svelte';
-  import { Toggle, Modal, Label, Input, Textarea, Select } from 'flowbite-svelte';
-  import { Icon } from 'flowbite-svelte-icons';
-  import { onMount } from 'svelte'
-  import { serverIP } from '$lib/localstore.js';
+import {
+    Icon,
+} from 'flowbite-svelte-icons';
 
-  // Initial values
-	let currentSensor  = "MA_TEMP";
-  let sensorData = [];
-  let AHUsensors = [];
-  let DropdownIsOpen = false;
-  let graphModeSelect = false; 
-  let selectDataButtonLabel = "zoom mode";
-  let formModal = false;
-  let selected;
-  let textareaprops = {
+import {
+    onMount
+} from 'svelte'
+
+import {
+    serverIP
+} from '$lib/localstore.js';
+
+// ------------------------ Page initialization -----------------------------
+onMount(async () => {
+    getAHUsensors();
+    await getSensorData();
+    updateGraph()
+});
+
+// ------------------------ UI elements -----------------------------
+let DropdownIsOpen = false;
+let graphModeSelect = false;
+let selectDataButtonLabel = "zoom mode";
+let showDataLabelForm = false;
+let showPopup = false;
+let textareaprops = {
     id: 'message',
     name: 'message',
     label: 'Your message',
     rows: 4,
     placeholder: 'Leave a comment...'
-  };
-  let faultLabel = [
-    { value: 'unfaulty', name: 'This data is un-faulty' },
-    { value: 'faulty', name: 'This data is faulty' },
-  ];
+};
 
-  // UI elements 
-  const dropdownClick = async (sensor) => {
-      currentSensor = sensor;
-      closeDropdown();
-      await getSensorData();
-      updateGraph();
-  }
-  const closeDropdown = () => {
-      DropdownIsOpen = false;
-  };
-
-  // API functions
-  const getAHUsensors = async () => {
-      const response = await fetch(`${serverIP}/AHU_sensor_info`)
-      const data = await response.json();
-      AHUsensors = data;
-  }
-  const getSensorData = async () => {
-      const response = await fetch(`${serverIP}/sensor_data?sensor=${currentSensor}`)
-      const data = await response.json();
-      sensorData = data;
-  }
-
-  // Page initialization
-  onMount(async () => {
-      getAHUsensors();
-      await getSensorData();
-      updateGraph()
-  });
-
-  // Graph
-
-  function selectPointsByDrag(e) {
-    config.series[0].zoneAxis = 'x';
-    config.xAxis.plotBands =  [
-          {from: e.xAxis[0].min, to: e.xAxis[0].max, color: 'rgba(69, 167, 255, 0.5)'},
-    ]
-
-    this.series.forEach(series => {
-      series.points.forEach(point => {
-        if (point.x >= e.xAxis[0].min && point.x <= e.xAxis[0].max) {
-          point.select(true, true);
-        }
-      });
-    });
-
-    // Fire a custom event
-    Highcharts.fireEvent(
-      this,
-      'selectedpoints', {
-        points: this.getSelectedPoints()
-      }
-    );
-
-    return false; // Don't zoom
-  }
-
-function toast(chart, text) {
-  chart.toast = chart.renderer.label(text, 100, 120)
-    .attr({
-      fill: Highcharts.getOptions().colors[0],
-      padding: 10,
-      r: 5,
-      zIndex: 8
-    })
-    .css({ color: '#FFFFFF' })
-    .add();
-
-  setTimeout(function() { chart.toast.fadeOut(); }, 2000);
-  setTimeout(function() { chart.toast = chart.toast.destroy(); }, 2500);
+const dropdownClick = async (sensor) => {
+    currentSensor = sensor;
+    closeDropdown();
+    await getSensorData();
+    updateGraph();
 }
-/*
- * The handler for a custom event, fired from selection event
- */
-function selectedPoints(e) {
-  // Show a label
-  toast(
-    this,
-    `<b>${e.points.length} points selected.</b>
-        <br>Click on empty space to deselect.`
-  );
+const closeDropdown = () => {
+    DropdownIsOpen = false;
+};
+
+const labelDataButton = () => {
+    if (selectedData == undefined) {
+        showPopup = true;
+        setTimeout(() => {
+            showPopup = false;
+        }, 5000);
+    } else {
+        showDataLabelForm = true;
+    }
+};
+
+function handleClick() {}
+
+// ------------------------ API functions ------------------------------
+
+let currentSensor = "MA_TEMP";
+let sensorData = [];
+let AHUsensors = [];
+
+const getAHUsensors = async () => {
+    const response = await fetch(`${serverIP}/AHU_sensor_info`)
+    const data = await response.json();
+    AHUsensors = data;
+}
+const getSensorData = async () => {
+    const response = await fetch(`${serverIP}/sensor_data?sensor=${currentSensor}`)
+    const data = await response.json();
+    sensorData = data;
 }
 
-/*
- * On click, unselect all points
- */
-function unselectByClick() {
-  config.series[0].zoneAxis = [];
-  config.xAxis.plotBands =  [];
-  const points = this.getSelectedPoints();
-  if (points.length > 0) {
-    points.forEach(point => point.select(false));
-  }
-}
+// ------------------------ Graph ------------------------------
 
+let selectedData = undefined;
 let config = {
     chart: {
         type: "line",
@@ -161,93 +128,198 @@ let config = {
     },
 
 };
-	
+
+// ------------------------ Graph click functions -----------
+
+function selectPointsByDrag(e) {
+    selectedData = e.xAxis;
+    config.series[0].zoneAxis = 'x';
+    config.xAxis.plotBands = [{
+        from: e.xAxis[0].min,
+        to: e.xAxis[0].max,
+        color: 'rgba(69, 167, 255, 0.5)'
+    }, ]
+
+    this.series.forEach(series => {
+        series.points.forEach(point => {
+            if (point.x >= e.xAxis[0].min && point.x <= e.xAxis[0].max) {
+                point.select(true, true);
+            }
+        });
+    });
+
+    Highcharts.fireEvent(
+        this,
+        'selectedpoints', {
+            points: this.getSelectedPoints()
+        }
+    );
+
+    return false;
+}
+
+function unselectByClick() {
+    selectedData = undefined;
+    config.series[0].zoneAxis = [];
+    config.xAxis.plotBands = [];
+    const points = this.getSelectedPoints();
+    if (points.length > 0) {
+        points.forEach(point => point.select(false));
+    }
+}
+
+function toast(chart, text) {
+    chart.toast = chart.renderer.label(text, 100, 120)
+        .attr({
+            fill: Highcharts.getOptions().colors[0],
+            padding: 10,
+            r: 5,
+            zIndex: 8
+        })
+        .css({
+            color: '#FFFFFF'
+        })
+        .add();
+
+    setTimeout(function() {
+        chart.toast.fadeOut();
+    }, 2000);
+    setTimeout(function() {
+        chart.toast = chart.toast.destroy();
+    }, 2500);
+}
+/*
+ * The handler for a custom event, fired from selection event
+ */
+function selectedPoints(e) {
+    // Show a label
+    toast(
+        this,
+        `<b>${e.points.length} points selected.</b>
+        <br>Click on empty space to deselect.`
+    );
+}
+
+// ------------------------ Graph UI changes -----------
 function updateGraph() {
-  config.series[0] = {data: sensorData['data'], name: sensorData['name']};
-  config.title.text = `${sensorData['name']}`;
-  config.subtitle.text = `Sensor label: ${sensorData['label']}`;
-  config.yAxis = {title: { text: `${sensorData['name']} (${sensorData['unit']})`}};
+    config.series[0] = {
+        data: sensorData['data'],
+        name: sensorData['name']
+    };
+    config.title.text = `${sensorData['name']}`;
+    config.subtitle.text = `Sensor label: ${sensorData['label']}`;
+    config.yAxis = {
+        title: {
+            text: `${sensorData['name']} (${sensorData['unit']})`
+        }
+    };
 };
 
 function selectDataClick() {
     if (graphModeSelect == false) {
-      config.chart.events = {
-              selection: selectPointsByDrag,
-              selectedpoints: selectedPoints,
-              click: unselectByClick
-              };
-      // config.chart.resetZoomButton.theme.style.display = 'none';
-      graphModeSelect = true;
-      selectDataButtonLabel = "select mode";
+        config.chart.events = {
+            selection: selectPointsByDrag,
+            selectedpoints: selectedPoints,
+            click: unselectByClick
+        };
+        graphModeSelect = true;
+        selectDataButtonLabel = "select mode";
     } else {
-      config.chart.events = {
-              selection: undefined,
-              selectedpoints: undefined,
-              click: undefined
-              };
-
-      // config.chart.resetZoomButton.theme.style.display = 'none';
-      graphModeSelect = false;
-      selectDataButtonLabel = "zoom mode";
+        config.chart.events = {
+            selection: undefined,
+            selectedpoints: undefined,
+            click: undefined
+        };
+        graphModeSelect = false;
+        selectDataButtonLabel = "zoom mode";
     }
 };
-
 </script>
 
 <main>
 
-{#if sensorData.length === 0}
+    {#if sensorData.length === 0}
     <br /><br />
     <br /><br />
     <Spinner size=20 />
 
-{:else}
+    {:else}
 
     <div class="left-dropdown">
-    <Button>Select sensor<Icon name="chevron-down-solid" class="w-2 h-3 ml-2 text-white dark:text-white" /></Button>
-    <Dropdown bind:open={DropdownIsOpen} class="overflow-y-auto py-1 h-64 whitespace-nowrap left-0" placement="bottom">
-        {#each Object.entries(AHUsensors) as [key, sensorName], index (key)}
+        <Button>Select sensor<Icon name="chevron-down-solid" class="w-2 h-3 ml-2 text-white dark:text-white" /></Button>
+        <Dropdown bind:open={DropdownIsOpen} class="overflow-y-auto py-1 h-64 whitespace-nowrap left-0" placement="bottom">
+            {#each Object.entries(AHUsensors) as [key, sensorName], index (key)}
             <DropdownItem on:click={() => dropdownClick(sensorName['label'])}>{sensorName['name']}</DropdownItem>
-        {/each}
-    </Dropdown>
+            {/each}
+        </Dropdown>
 
     </div>
 
     <br /><br />
     <div class="chart" use:chart={config}></div>
 
-    <Toggle on:change={() => selectDataClick()}>Select mode</Toggle>
-    <div class="left-dropdown">
-
-    <Badge color="green">{`Mode: ${selectDataButtonLabel}`}</Badge>
-  
+    <div class="left-dropdown" style="display: flex; gap: 10px;">
+        <Button on:click={() => labelDataButton()}>Set data label</Button>
+        <Toggle on:change={() => selectDataClick()}>Toggle data selection mode</Toggle>
+        <br /><br />
     </div>
 
-    <div class="left-dropdown">
-    <Button on:click={() => (formModal = true)}>Form modal</Button>
-    </div>
+    {/if}
 
-
-{/if}
-    <Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
-      <form class="flex flex-col space-y-6" action="#">
-        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Label Data</h3>
-        <Label class="space-y-2">
-          Select an option
-          <Select class="mt-2" items={faultLabel} bind:value={selected} />
-        </Label>
-        <Label class="space-y-2">
-          <span>Notes</span>
-          <Textarea {...textareaprops} />
-        </Label>
-        <Button type="submit" class="w-full1">Submit</Button>
-      </form>
+    <!------------------------------- Popup elements ---------------------------------------->
+    <Modal bind:open={showDataLabelForm} size="xs" autoclose={false} class="w-full">
+        <form class="flex flex-col space-y-6">
+            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Label Data</h3>
+            <Label class="space-y-2">
+                Select an option
+                <Radio name="example" checked={true}>Faulty Data</Radio>
+                <Radio name="example">Unfaulty Data</Radio>
+                <Radio name="example">Remove all labels from selection</Radio>
+            </Label>
+            <Label class="space-y-2">
+                <span>Notes</span>
+                <Textarea {...textareaprops} />
+            </Label>
+            <Button type="submit" class="w-full1">Submit</Button>
+        </form>
     </Modal>
+
+    {#if showPopup}
+    <div class="popup">
+        <p>No data selected</p>
+    </div>
+    {/if}
 
 </main>
 
 <style>
 .left-dropdown {
     text-align: left;
+}
+
+.popup {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background-color: #1F2937;
+    /* Red */
+    color: #F98080;
+    padding: 16px;
+    margin: 20px;
+    border-radius: 5px;
+    z-index: 1000;
+    /* Sit on top */
+    animation-name: fadein;
+    animation-duration: 0.5s;
+}
+
+@keyframes fadein {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
 }
 </style>
