2b086244b77d3ce9f81c8806b2ec4b44924fd0fd
connbrack <connor.brackley@mail.concordia.ca>
2023-10-16 20:49:51 -0700

    auto activate conda env

--- launch.sh
+++ launch.sh
@@ -1,13 +1,23 @@
 #bin/bash
 
+# Activate conda capabilities
+eval "$(conda shell.bash hook)"
+source ~/Apps/miniconda3/etc/profile.d/conda.sh
+
+# check if screen is running and quit if it is
 if screen -list | grep -q "website"; then
     screen -X -S "website" quit
 fi 
+
+# Launch the website and server
 
 cd website &&
 screen -dmS website bash -c "npm run dev" &&
 cd .. &&
 
 cd server &&
+conda activate IML-app &&
 python server.py &&
+
+# Close sreen when server is closed
 screen -X -S "website" quit
